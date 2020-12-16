1af354fd90f12a523b77c4a3c738ea462cd7894c
daiqian <daiqian@changingedu.com>
2020-12-16 13:23:45 +0800

    for循环应用及局部变量使用范围

---
+++ src/main/java/com/imooc/flow1/ForDemo.java
@@ -0,0 +1,15 @@
+package com.imooc.flow1;
+
+/**
+ * Created by daiqian on 2020-12-1613:20.
+ *
+ */
+public class ForDemo {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int n=1;n<=5;n++){
+            sum=sum+n;
+        }
+        System.out.println("sum="+sum);
+    }
+}
