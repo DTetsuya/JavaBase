8c51bdb92e9527e0c514e3fb3cf71d79440b8b44
daiqian <daiqian@changingedu.com>
2020-12-16 13:39:51 +0800

    for循环的注意事项

--- src/main/java/com/imooc/flow1/ForDemo.java
+++ src/main/java/com/imooc/flow1/ForDemo.java
@@ -2,7 +2,7 @@ package com.imooc.flow1;
 
 /**
  * Created by daiqian on 2020-12-1613:20.
- *
+ *局部变量只在定义它的大括号内有效
  */
 public class ForDemo {
     public static void main(String[] args) {
---
+++ src/main/java/com/imooc/flow1/ForDemo1.java
@@ -0,0 +1,18 @@
+package com.imooc.flow1;
+
+/**
+ * Created by daiqian on 2020-12-1613:26.
+ */
+public class ForDemo1 {
+    public static void main(String[] args) {
+        int i=1;
+        //将10以下的整数输出
+        //for (;;)
+         while(true){
+            System.out.print(i+" ");
+            if(i==10)break;
+            i++;
+        }
+    }
+
+}
